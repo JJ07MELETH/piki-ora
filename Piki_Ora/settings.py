fafacebb86d876b734671d52cbb878049ff149cb
JJ07MELETH <133310668+JJ07MELETH@users.noreply.github.com>
2026-05-05 01:15:30 +1200

    Fix ALLOWED_HOSTS for Vercel

--- Piki_Ora/settings.py
+++ Piki_Ora/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m*62=8xx6)7_=za08!f@^4w+h83%(e08#f8*b6ba=*nc2jm&x$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'piki-ora-eight.vercel.app', '.vercel.app']
 
 
 # Application definition
@@ -122,3 +122,7 @@ AUTH_USER_MODEL = 'Appointments.User'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'https://piki-ora-eight.vercel.app',
+    'https://*.vercel.app',
+]
